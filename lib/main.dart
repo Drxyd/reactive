2144197b2f4cef8de8dce90525788c469cd6a8be
Humphrey Kganakga <xydhumphrey1@gmail.com>
2024-09-10 15:41:26 +0200

    Stateful Reactor And Shield
    The new stateful reactor build itself based on data provided to it by the reactWith method or the data field.
    
    Sheilf catches errors in the widget tree and builds an error state UI, it is possible to use shield to log and gracefully recover from error states
    
    The old reactor has been renamed to catalyst.
    
    rebuild has been renamed to react

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,1 @@
-export 'src/reactive.dart' show 
-    Reactive, Core, Optic,
-    Reactor, ReactorCore;
+export 'src/reactive.dart' show Reactive, Core, Build, Reactor, Catalyst, Shield;
--- lib/src/reactive.dart
+++ lib/src/reactive.dart
@@ -1,49 +1,34 @@
-// Made more changes to save on key strokes.
-
 library reactive;
 
 import 'package:flutter/material.dart';
-
-// A more transparent interface to facilitate an imperative style
-// Replaces StatefulWidget
-abstract class Core
-{
-	Key? key;
-
-	Core({this.key});
-
-	late bool Function() isMounted;
-	late void Function() _rebuild;
-	bool isInitialized = false;
-
-	void rebuild () { if(isInitialized) _rebuild(); }
-
-	void registerUpdate(void Function(void Function()) setState, bool Function() isMounted) 
-	{
-		this.isMounted = isMounted;
-		_rebuild = () {
-			if(isMounted()) {
-				setState((){});
-			}
-		};
-		isInitialized = true;
-	}
-}
 
 abstract class Reactive <T extends Core> extends StatefulWidget
 {
 	final T core;
 
-	Reactive({Key? key, required this.core}) 
-		: super(key: key ?? core.key);
+	const Reactive({super.key, required this.core});
 
-	void rebuild () { core.rebuild(); }
+	void react () { if(core.isInitialized) core.rebuild(); }
 
 	@override
 	State<Reactive> createState();
 }
 
-abstract class Optic <T extends Core> extends State<Reactive<T>>
+abstract class Core
+{
+	late bool Function() isMounted;
+	late void Function() rebuild;
+	bool isInitialized = false;
+
+	void linkBuild(void Function(void Function()) setState, bool Function() isMounted) 
+	{
+		this.isMounted = isMounted;
+		rebuild = () { if(isMounted()) setState((){}); };
+		isInitialized = true;
+	}
+}
+
+abstract class Build <T extends Core> extends State<Reactive<T>>
 {
 	late T core;
 
@@ -53,7 +38,7 @@ abstract class Optic <T extends Core> extends State<Reactive<T>>
 	{
 		super.initState();
 		core = widget.core;
-		core.registerUpdate(setState, () => mounted);
+		core.linkBuild(setState, () => mounted);
 	}
 
 	@override
@@ -61,27 +46,122 @@ abstract class Optic <T extends Core> extends State<Reactive<T>>
 }
 
 
-// Builder based interface to facilitate a more declarative style
-// Replaces StatelessWidget
-class Reactor extends Reactive<ReactorCore>
+class Catalyst extends Reactive<CatalystCore>
 {
-	Reactor({super.key, required this.builder}) 
-		: super(core: ReactorCore(builder: builder));
-
 	final Widget Function() builder;
 
+	Catalyst({super.key, required this.builder}) 
+		: super(core: CatalystCore(builder: builder));
+
 	@override
-	ReactorOptic createState() => ReactorOptic();
+	CatalystBuild createState() => CatalystBuild();
 }
 
-class ReactorCore extends Core
+class CatalystCore extends Core
 {
-	Widget Function() builder;
-	ReactorCore({required this.builder});
+	final Widget Function() builder;
+
+	CatalystCore({required this.builder});
 }
 
-class ReactorOptic extends Optic<ReactorCore>
+class CatalystBuild extends Build<CatalystCore>
 {
 	@override
 	Widget build(BuildContext context) => core.builder();
+}
+
+
+class Reactor<T> extends Reactive<ReactorCore<T?>>
+{
+	Reactor({super.key, required Widget Function(T?) builder, T? data}) 
+		: super(core: ReactorCore<T>(builder: builder, data: data));
+
+	void reactWith(T newData)
+	{
+		if(core.data.hashCode != newData.hashCode)
+		{
+			core.data = newData;
+			react();
+		}
+	}
+
+	T? coreDump() => core.data;
+
+	@override
+	ReactorBuild<T> createState() => ReactorBuild<T>();
+}
+
+class ReactorCore<T> extends Core
+{
+	final Widget Function(T?) builder;
+	T? data;
+
+	ReactorCore({required this.builder, required this.data});
+}
+
+class ReactorBuild<T> extends Build<ReactorCore<T>>
+{
+	@override
+	Widget build(BuildContext context) => core.builder(core.data);
+}
+
+
+class Shield extends Reactive<ShieldCore> 
+{
+	Shield
+	({
+		super.key,
+		required Widget child,
+		required Widget Function(Object error, StackTrace stackTrace) fallbackBuilder,
+	}) : super(core: ShieldCore(fallbackBuilder: fallbackBuilder, child: child));
+
+	void setError(Object error, StackTrace stackTrace) =>
+		core.setError(error, stackTrace);
+
+	void clearError() =>
+		core.clearError();
+
+	@override
+	ShieldBuild createState() => ShieldBuild();
+}
+
+class ShieldCore extends Core 
+{
+	final Widget Function(Object error, StackTrace stackTrace) fallbackBuilder;
+	final Widget child;
+
+	ShieldCore
+	({
+		required this.fallbackBuilder, 
+		required this.child
+	});
+	
+	Object? error;
+	StackTrace? stackTrace;
+
+	void setError(Object error, StackTrace stackTrace) 
+	{
+		this.error = error;
+		this.stackTrace = stackTrace;
+		rebuild();
+	}
+
+	void clearError() 
+	{
+		error = null;
+		stackTrace = null;
+		rebuild();
+	}
+}
+
+class ShieldBuild extends Build<ShieldCore> 
+{
+	@override
+	Widget build(BuildContext context) 
+	{
+		if (core.error != null && core.stackTrace != null) {
+			return core.fallbackBuilder(core.error!, core.stackTrace!);
+		}
+		return core.child;
+	}
 }
