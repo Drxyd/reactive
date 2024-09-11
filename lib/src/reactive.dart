4c5fb4b0af9d554860a0e72dd769599467c46203
Humphrey Kganakga <xydhumphrey1@gmail.com>
2024-09-11 23:49:01 +0200

    Reactor reimplemented against StatefulWidget

--- lib/src/reactive.dart
+++ lib/src/reactive.dart
@@ -79,10 +79,17 @@ class CatalystBuild extends Build<CatalystCore>
 }
 
 
-class Reactor<T> extends Reactive<ReactorCore<T?>>
+class Reactor<T> extends StatefulWidget
 {
+	final ReactorCore<T> core;
 	Reactor({super.key, required Widget Function(T?) builder, T? data}) 
-		: super(core: ReactorCore<T>(builder: builder, data: data));
+		: core = ReactorCore<T>(builder: builder, data: data);
+
+	void react () { 
+		bool initialized = core.isInitialized;
+		assert(initialized, "react called on unmounted widget");
+		if(initialized) core.rebuild(); 
+	}
 
 	void reactWith(T newData)
 	{
@@ -99,16 +106,42 @@ class Reactor<T> extends Reactive<ReactorCore<T?>>
 	ReactorBuild<T> createState() => ReactorBuild<T>();
 }
 
-class ReactorCore<T> extends Core
+class ReactorCore<T>
 {
 	final Widget Function(T?) builder;
 	T? data;
 
 	ReactorCore({required this.builder, required this.data});
+
+	late bool Function() isMounted;
+	late void Function() rebuild;
+	bool isInitialized = false;
+
+	void linkBuild(void Function(void Function()) setState, bool Function() isMounted) 
+	{
+		this.isMounted = isMounted;
+		rebuild = () { 
+			bool mounted = isMounted();
+			assert(mounted, "rebuild called on unmounted widget");
+			if(mounted) setState((){}); 
+		};
+		isInitialized = true;
+	}
 }
 
-class ReactorBuild<T> extends Build<ReactorCore<T>>
+class ReactorBuild<T> extends State<Reactor<T>>
 {
+	late ReactorCore<T> core;
+
+	@override
+	@mustCallSuper
+	void initState()
+	{
+		super.initState();
+		core = widget.core;
+		core.linkBuild(setState, () => mounted);
+	}
+	
 	@override
 	Widget build(BuildContext context) => core.builder(core.data);
 }
